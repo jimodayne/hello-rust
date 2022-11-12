12e342891a18b4286f59606fe1653c9f67142d59
Thinh Tran <tio212@gmail.com>
2022-11-12 21:56:05 +0200

    feat: add guessing game

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use core::num;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..10);
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed {}", guess);
+
+        let order = guess.cmp(&secret_number);
+
+        match order {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You won!");
+                break;
+            }
+        }
+    }
+
+    // println!("The secret number is: {secret_number}");
+}
